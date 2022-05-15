e74c8a9409c19a49639572b7fdc8f706a56ca2b3
dianKoliva <dianpretty01@gmail.com>
2022-05-15 13:08:32 +0200

    new changes

--- App.jsx
+++ App.jsx
@@ -1,9 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {tailwind} from 'tailwind-rn';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
+      
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
