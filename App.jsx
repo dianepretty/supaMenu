f0c9363a32d17bbaf7f8e8363515a53148521ae9
Tracy Murenzi <tracymurenzi@gmail.com>
2022-05-16 22:36:55 +0200

    init

--- App.jsx
+++ App.jsx
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import {tailwind} from 'tailwind-rn';
+import Landing from './screens/Landing';
 import Menu from './screens/Menu';
 import RateUs from './screens/RateUs';
 import SignUp from './screens/SignUp';
--- screens/Landing.jsx
+++ screens/Landing.jsx
@@ -4,13 +4,35 @@ import React, { Component } from 'react'
 export default class Landing extends Component {
   render() {
     return (
-      <View>
-        <Text>Landing</Text>
+      <View style={styles.container}>
+      <View style={styles.title}>
+        <Text style={styles.supa}>Supa</Text>
+        <Text style={styles.menu}>Menu</Text>
+        </View>
       </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
-    
+    container:{
+        backgroundColor:"orange",
+        height: "100%",
+    },
+    title:{
+        flexDirection:"row",
+        alignSelf:"center",
+        marginTop: "80%"
+        
+      },
+      supa:{
+          fontSize:"37px",
+          fontWeight:"700",
+  
+      },
+      menu:{
+          fontSize:"37px",
+          fontWeight:"700",
+          color:"#fff",
+      },
 })
