e55ba69fd97b11f4adc90cd80dab0ce8201c9a2b
Kellia U <kellumuhire@gmail.com>
2022-05-18 21:08:15 +0200

    Resto&Restos and header

--- App.jsx
+++ App.jsx
@@ -1,23 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import {tailwind} from 'tailwind-rn';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Left from "./src/components/Header/Left";
+import HeaderSearch from "./src/components/Header/SearchBar";
+import Resto from "./src/screens/Resto";
+import Restos from "./src/screens/Restos";
 
+// import {
+//   useFonts,
+//   Arvo_400Regular,
+//   Arvo_700Bold,
+//   Rubik_400Regular,
+// } from "@expo-google-fonts/dev";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Restos">
+        <Stack.Screen
+          name="Restos"
+          component={Restos}
+          options={
+            {
+              title: "Restaurants",
+              headerLeft: ({ navigation }) => {
+                return <Left onPress={() => navigation.goBack()} />;
+              },
+              headerTitle: (props) => <HeaderSearch {...props} />,
+            }
+            // ...coptions,
+          }
+        />
+        <Stack.Screen
+          name="Resto"
+          component={Resto}
+          options={{
+            title: "Restaurant",
+            headerLeft: ({ navigation }) => {
+              return (
+                <Left onPress={() => navigation.navigation("Restaurants")} />
+              );
+            },
+            headerTitle: (props) => null,
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
