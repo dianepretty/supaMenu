df6cc0401d6e9ae806e5f3966fec72064dc04517
Tracy Murenzi <tracymurenzi@gmail.com>
2022-05-19 19:44:13 +0200

    init

---
+++ src/screens/LandingPage.jsx
@@ -0,0 +1,14 @@
+import { Text, StyleSheet, View } from 'react-native'
+import React, { Component } from 'react'
+
+export default class LandingPage extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore commodi magni molestiae at provident ullam et voluptatum fugiat vitae perspiciatis, delectus nesciunt deleniti sunt hic voluptate ut quis, sapiente quod.</Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({})
