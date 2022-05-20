2c05c38d6b73048f6ba6e8d248a5723d46c669bb
Tracy Murenzi <tracymurenzi@gmail.com>
2022-05-20 22:34:43 +0200

    init

--- App.jsx
+++ App.jsx
@@ -2,16 +2,11 @@ import {NavigationContainer} from '@react-navigation/native'
 import LoginPage from "./src/screens/LoginPage";
 import SignUpPage from "./src/screens/SignUpPage";
 import LandingPage from "./src/screens/LandingPage";
-import Tabs from './src/navigation/Tabs'
+import Tabs from './src/navigation/Tabs';
+import Payement from "./src/screens/Payement";
 
 export default function App() {
-<<<<<<< HEAD
-  return <LoginPage/> ;
-=======
   return (
-  <NavigationContainer>
-      <Tabs />
-    </NavigationContainer>
+  <Payement/>
   );
->>>>>>> 2523a6741b16f7fd2735094996b548d438b03765
 }
