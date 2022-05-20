4aed193bbee510d70091154358b75325e99d31b9
aimee-annabelle <inezannabelle@gmail.com>
2022-05-20 16:16:07 +0200

    resolved confilcts

--- metro.config.js
+++ metro.config.js
@@ -1,20 +1,3 @@
-// const { getDefaultConfig } = require("metro-config");
-
-// module.exports = (async () => {
-//   const {
-//     resolver: { sourceExts, assetExts }
-//   } = await getDefaultConfig();
-//   return {
-//     transformer: {
-//       babelTransformerPath: require.resolve("react-native-svg-transformer")
-//     },
-//     resolver: {
-//       assetExts: assetExts.filter(ext => ext !== "svg"),
-//       sourceExts: [...sourceExts, "svg"]
-//     }
-//   };
-// })();
-
 /**
  * Metro configuration for React Native with svg support
  * https://github.com/facebook/react-native
