2cce33d085c0f81c198c6be59b0b94fe10690159
Kellia U <kellumuhire@gmail.com>
2022-05-21 18:57:43 +0200

    Replace svg with image

--- src/screens/ScanScreen.jsx
+++ src/screens/ScanScreen.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-import { Text, View, StyleSheet } from "react-native";
-import QrCode from "../../assets/icons/qr-code-svgrepo-com.svg";
-// import SvgUri from "react-native-svg";
+import { Text, View, StyleSheet, Image } from "react-native";
 import SearchBarComponent from "../components/SearchBarComponent";
 
 const ScanScreen = () => {
@@ -9,7 +7,10 @@ const ScanScreen = () => {
     <View style={Styles.body}>
       <SearchBarComponent />
       <Text style={Styles.scanText}>or</Text>
-      <QrCode width={80} height={80} fill="#000" style={Styles.scanPic} />
+      <Image
+        source={require("../../assets/icons/qr-code-svgrepo-com.svg")}
+        style={Styles.scanPic}
+      />
       <Text style={Styles.scanText}>Scan, Pay & Enjoy!</Text>
     </View>
   );
@@ -21,7 +22,6 @@ const Styles = StyleSheet.create({
     height: "100%",
     alignItems: "center",
     padding: 40,
-    top: 30,
   },
   scanText: {
     fontWeight: "bold",
@@ -31,6 +31,8 @@ const Styles = StyleSheet.create({
   },
   scanPic: {
     marginTop: 100,
+    width: 80,
+    height: 80,
   },
 });
 
