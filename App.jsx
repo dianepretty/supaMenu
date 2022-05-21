89a76fbe53ef18b3ada4b241c20ba0e30a586706
dianKoliva <dianpretty01@gmail.com>
2022-05-21 23:33:15 +0200

    new changes

--- App.jsx
+++ App.jsx
@@ -3,10 +3,11 @@ import { NavigationContainer } from "@react-navigation/native";
 import Tabs from "./src/navigation/Tabs";
 import LandingPage from "./src/screens/LandingPage";
 import AuthStack from "./src/navigation/AuthStack";
+import Cart from "./src/screens/Cart";
 
 export default function App() {
   const [appIsReady, setAppIsReady] = useState(false);
-  const [auth, setAuth] = useState(false);
+  const [auth, setAuth] = useState(true);
 
   useEffect(() => {
     setTimeout(() => {
@@ -15,7 +16,7 @@ export default function App() {
   }, []);
 
   return appIsReady ? (
-    <NavigationContainer>{auth ? <Tabs /> : <AuthStack />}</NavigationContainer>
+    <NavigationContainer>{auth ? <Tabs /> : <AuthStack/>}</NavigationContainer>
   ) : (
     <LandingPage />
   );
